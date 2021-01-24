56715a7ec48b75dafd643fb752b21b229e522619
Ida Bagus Kadek Darma Wiryatama <wirya_tama99@hotmail.com>
2021-01-24 20:21:33 +0800

    View Pesan

--- resources/views/pages/home/pesan.blade.php
+++ resources/views/pages/home/pesan.blade.php
@@ -29,44 +29,100 @@
         </div>
     @endif
     
-    <form action="{{ route('sms.send') }}" method="post">
-        @csrf
-        <!-- Receiever -->
-        <section>
-            <x-input-form label="Penerima">
-                <select name="customer_id" class="form-control">
-                    <option value="" hidden>--Pilih pelanggan</option>
-                    @foreach ($customer as $item)
-                        <option value="{{ $item->customer_id }}">{{ $item->name . ' - ' . $item->phone }}</option>
-                    @endforeach
-                </select>
-            </x-input-form>
-        </section>
-        <!--/ Receiever -->
-
-        <!-- Template -->
-        <section>
-            <x-input-form label="Template">
-                <select name="template" class="form-control" id="template">
-                    <option value="" hidden>--Pilih template</option>
-                    @foreach ($template as $item)
-                        <option value="{{ $item->id }}">{{ $item->title }}</option>
-                    @endforeach
-                </select>
-            </x-input-form>
-        </section>
-        <!--/ Template -->
-
-        <!-- Message -->
-        <section>
-            <x-input-form label="Tulis Pesan">
-                <textarea class="form-control" name="message" id="message" cols="30" rows="5" placeholder="Tulis Pesan"></textarea>
-            </x-input-form>
-        </section>
-        <!--/ Message -->
+    <x-card-basic title="Pesan Baru" class="">
+        <x-slot name="floatRight"></x-slot>
         
-        <button type="submit" class="btn btn-primary">Kirim</button>
-    </form>
+        <x-tab class="">
+            <x-slot name="menu">
+                <li class="nav-item">
+                    <a class="nav-link active" id="pesan-tab" data-toggle="tab" href="#pesan" role="tab" aria-controls="pesan" aria-selected="true">Pesan</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" id="grup-tab" data-toggle="tab" href="#grup" role="tab" aria-controls="grup" aria-selected="false">Grup</a>
+                  </li>
+            </x-slot>
+            <div class="tab-pane fade show active" id="pesan" role="tabpanel" aria-labelledby="pesan-tab">
+                <form action="{{ route('sms.send') }}" method="post">
+                    @csrf
+                    <!-- Receiever -->
+                    <section>
+                        <x-input-form label="Penerima">
+                            <select name="customer_id" class="form-control">
+                                <option value="" hidden>--Pilih pelanggan</option>
+                                @foreach ($customer as $item)
+                                    <option value="{{ $item->customer_id }}">{{ $item->name . ' - ' . $item->phone }}</option>
+                                @endforeach
+                            </select>
+                        </x-input-form>
+                    </section>
+                    <!--/ Receiever -->
+            
+                    <!-- Template -->
+                    <section>
+                        <x-input-form label="Template">
+                            <select name="template" class="form-control" id="template">
+                                <option value="" hidden>--Pilih template</option>
+                                @foreach ($template as $item)
+                                    <option value="{{ $item->id }}">{{ $item->title }}</option>
+                                @endforeach
+                            </select>
+                        </x-input-form>
+                    </section>
+                    <!--/ Template -->
+            
+                    <!-- Message -->
+                    <section>
+                        <x-input-form label="Tulis Pesan">
+                            <textarea class="form-control" name="message" id="message" cols="30" rows="5" placeholder="Tulis Pesan"></textarea>
+                        </x-input-form>
+                    </section>
+                    <!--/ Message -->
+                    
+                    <button type="submit" class="btn btn-primary">Kirim</button>
+                </form>
+            </div>
+            <div class="tab-pane fade" id="grup" role="tabpanel" aria-labelledby="grup-tab">
+                <form action="{{ route('sms.send') }}" method="post">
+                    @csrf
+                    <!-- Receiever -->
+                    <section>
+                        <x-input-form label="Penerima">
+                            <select name="customer_id" class="form-control">
+                                <option value="" hidden>--Pilih pelanggan</option>
+                                @foreach ($customer as $item)
+                                    <option value="{{ $item->customer_id }}">{{ $item->name . ' - ' . $item->phone }}</option>
+                                @endforeach
+                            </select>
+                        </x-input-form>
+                    </section>
+                    <!--/ Receiever -->
+            
+                    <!-- Template -->
+                    <section>
+                        <x-input-form label="Template">
+                            <select name="template" class="form-control" id="template">
+                                <option value="" hidden>--Pilih template</option>
+                                @foreach ($template as $item)
+                                    <option value="{{ $item->id }}">{{ $item->title }}</option>
+                                @endforeach
+                            </select>
+                        </x-input-form>
+                    </section>
+                    <!--/ Template -->
+            
+                    <!-- Message -->
+                    <section>
+                        <x-input-form label="Tulis Pesan">
+                            <textarea class="form-control" name="message" id="message" cols="30" rows="5" placeholder="Tulis Pesan"></textarea>
+                        </x-input-form>
+                    </section>
+                    <!--/ Message -->
+                    
+                    <button type="submit" class="btn btn-primary">Kirim</button>
+                </form>
+            </div>
+        </x-tab>
+    </x-card-basic>
 @endsection
 
 @section('js')
